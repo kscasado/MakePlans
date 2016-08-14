9485744a18f0f351e4475b30db3912d5f2af1060
Kevin Casado <KevinDesktop@Kevins-Mac-mini.local>
2016-08-13 23:31:28 -0700

    added eslint

--- server.js
+++ server.js
@@ -10,3 +10,23 @@ const app = express()
 app.use(bodyParser.json())
 
 const developmentMode = process.env.NODE_ENV
+
+const isDeveloping = process.env.NODE_ENV !== 'production'
+  console.log(isDeveloping)
+  if (isDeveloping) {
+    app.use(middleware)
+    app.use(webpackHotMiddleware(compiler))
+    app.get('*', function response (req, res) {
+      res.write(middleware.fileSystem.readFileSync(path.join(__dirname, '../dist/index.html')))
+      res.end()
+    })
+  } else {
+    app.use(compression())
+    app.use(express.static(path.join(__dirname, '../dist')))
+    app.get('*', function response (req, res) {
+      res.sendFile(path.join(__dirname, '../dist/index.html'))
+    })
+  }
+  const port = 3000
+  app.listen(port, () => console.log(`Running on port ${port}`))
+})()
